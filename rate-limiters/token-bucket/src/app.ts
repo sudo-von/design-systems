f0e072793c205a9b8b621148c33a282659b5ca93
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2025-03-06 20:36:25 -0600

    feat: :sparkles: add middleware to express

--- rate-limiters/token-bucket/src/app.ts
+++ rate-limiters/token-bucket/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { rateLimitMiddleware } from './middleware';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use(rateLimitMiddleware);
 
 app.get('/', (_req, res) => {
   res.status(200).send({
